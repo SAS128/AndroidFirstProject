ce35de59a67a145433266eaade0fc2345b184561
student <Sandmen461@gmail.com>
2021-02-04 20:49:15 +0200

    Goog star V progect 26 phone 26

--- app/src/main/java/com/example/androidfirstproject/MainActivity.java
+++ app/src/main/java/com/example/androidfirstproject/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.androidfirstproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView textView = (TextView)findViewById(R.id.textView52);
+        textView.setOnClickListener(new TextView.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //TextView.generateViewId(Integer.parseInt("fefe"));
+            }
+
+
+        });
     }
 }
