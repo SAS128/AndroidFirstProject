2ec43fb6618884c758d995bdb1d48c06c54b0678
SAS128 <57214862+SAS128@users.noreply.github.com>
2021-02-07 13:39:43 +0200

    Калькулятор первые рабочие кнопки

--- app/src/main/java/com/example/androidfirstproject/MainActivity.java
+++ app/src/main/java/com/example/androidfirstproject/MainActivity.java
@@ -12,11 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView textView = (TextView)findViewById(R.id.textView52);
-        textView.setOnClickListener(new TextView.OnClickListener(){
+        final TextView btn_1text = (TextView)findViewById(R.id.btn_1);
+        final TextView btn_inp = (TextView) findViewById(R.id.btn_inpit);
+        final TextView btn_2text = (TextView) findViewById(R.id.btn_2);
+        btn_1text.setOnClickListener(new TextView.OnClickListener(){
             @Override
             public void onClick(View v) {
-                //TextView.generateViewId(Integer.parseInt("fefe"));
+
+                 btn_inp.setText(  btn_inp.getText().toString() +"1"); //set text for text view
+            }
+
+
+        });
+        btn_2text.setOnClickListener(new TextView.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                btn_inp.setText("2"); //set text for text view
             }
 
 
